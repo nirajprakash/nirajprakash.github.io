46d9ceeacea1c4188f16763fcc114f65f8ec8481
nirajprakash <nirajprakash13@gmail.com>
2022-10-07 10:51:36 +0530

    added about

--- src/app/ui/pages/home/home-about/home-about.component.ts
+++ src/app/ui/pages/home/home-about/home-about.component.ts
@@ -1,15 +1,111 @@
-import { Component, OnInit } from '@angular/core';
+import { ScrollDispatcher, ViewportRuler } from '@angular/cdk/scrolling';
+import { ChangeDetectorRef, Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { MediaObserver } from '@angular/flex-layout';
+import { takeUntil, startWith, map, scan, distinctUntilChanged, takeWhile, switchMap, Observable, ReplaySubject } from 'rxjs';
+import { ENTER_SCALE, TRANSITION_AREA_SLIDE, TRANSITION_IMAGE_SCALE, TRANSITION_TEXT } from 'src/app/ui/animations/transitions/transitions.constants';
+import { UiUtilsView } from 'src/app/ui/utils/views.utils';
 
 @Component({
   selector: 'app-home-about',
   templateUrl: './home-about.component.html',
-  styleUrls: ['./home-about.component.scss']
+  styleUrls: ['./home-about.component.scss'],
+  animations: [
+    TRANSITION_TEXT,
+    TRANSITION_AREA_SLIDE,
+    TRANSITION_IMAGE_SCALE,
+    ENTER_SCALE
+  ]
 })
 export class HomeAboutComponent implements OnInit {
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  mOnceAnimated = false
+ 
+  /* ********************************************************************************************
+  *                anims
+  */
+  _mTriggerAnim?= 'false'
 
-  constructor() { }
+  _mTriggerImage?= 'false'
+
+
+  _mThreshold = 0.2
+
+  
+  @ViewChild('animRefView') vAnimRefView?: ElementRef<HTMLElement>;
+  
+  constructor(public el: ElementRef,
+    private _ngZone: NgZone,
+    private cdr: ChangeDetectorRef,
+    public mediaObserver: MediaObserver,
+    private scroll: ScrollDispatcher, private viewPortRuler: ViewportRuler) { }
 
   ngOnInit(): void {
+  }
+
+  
+
+  ngAfterViewInit(): void {
+    this.setupAnimation();
+  }
+
+  ngOnDestroy(): void {
+    
+    this.destroyed$.next(true)
+    this.destroyed$.complete()
+  }
+
+
+  public setupAnimation() {
+    if (!this.vAnimRefView) return;
+
+    // console.info("home products setupAnimation: " )
+    this.scroll.ancestorScrolled(this.vAnimRefView, 100).pipe(
+      // Makes sure to dispose on destroy
+      takeUntil(this.destroyed$),
+      startWith(0),
+      map(() => {
+        if (this.vAnimRefView != null) {
+          var visibility = UiUtilsView.getVisibility(this.vAnimRefView, this.viewPortRuler)
+          // console.log("product app-item UiUtilsView visibility: ", visibility)
+          return visibility;
+        }
+        return 0;
+
+      }),
+      scan<number, boolean>((acc: number | boolean, val: number) => (val >= this._mThreshold || (acc ? val > 0 : false))),
+      // Distincts the resulting triggers 
+      distinctUntilChanged(),
+      // Stop taking the first on trigger when aosOnce is set
+      takeWhile(trigger => {
+        // console.info("app-item  !trigger || !this.mOnceAnimated",
+        //   !trigger || !this.mOnceAnimated)
+
+        return !trigger || !this.mOnceAnimated
+      }, true),
+      switchMap(trigger => new Observable<number | boolean>(observer => this._ngZone.run(() => observer.next(trigger))))
+    ).subscribe(val => {
+
+
+      // console.log("home-item setupAnimation ancestorScrolled: ", val)
+
+      if (this.mOnceAnimated) {
+        return;
+      }
+
+      if (val) {
+        // console.log("HomeProductsComponent setupAnimation setupAnimation ancestorScrolled: ", val)
+
+        this.mOnceAnimated = true
+        this._mTriggerAnim = 'true'
+        this.cdr.detectChanges()
+      }
+      // if (this.vImageArea != null) {
+      //   var visibility = UiUtilsView.getVisibility(this.vImageArea, this.viewPortRuler)
+      //   console.log("UiUtilsView visibility: ", visibility)
+      // }
+    }
+
+    )
   }
 
 }
